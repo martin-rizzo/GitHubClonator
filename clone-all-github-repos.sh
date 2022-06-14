bcef172c4243112591e96f588b6b98b68ddca063
Martin Rizzo <martinrizzo@gmail.com>
2022-06-14 20:49:16 -0300

    feat: use repo owner as username when no username is supplied

--- clone-all-github-repos.sh
+++ clone-all-github-repos.sh
@@ -12,8 +12,8 @@ Options:
         
     -s, --ssh        Clone repos using ssh (SSH keys must be configured)
     -n, --dry-run    Do not actually run any commands; just print them
-    -l, --list       List the user repositories
-    -d, --debug      Print internal info about each repo
+    -l, --list       List user repositories
+    -d, --debug      List user repositories with detailed info
 
     -gt, --by-topic  Group repos in dirs based on its topics (default)
     -gl, --by-list   Group repos in dirs based on stars list
@@ -28,6 +28,7 @@ MaxDirLength=64           # maximum directory length (0 = no limit)
 BaseDir=                  # base directory where the repos will be stored
 DryRun=                   # set this var to 'echo' to do a dry-run
 UserName=                 # name used for the github user account
+UserToken=
 Command='clone_all_repos' # main command to execute
 Group='--by-topic'        # method used to group repositories
 ScriptName=${0##*/}       # name of this script
@@ -52,23 +53,23 @@ fatal_error() {
 clone_all_repos() {
     [ -z "$UserName" ] && show_help && exit 0
     [ -e "${BaseDir%/}" ] && fatal_error "directory '${BaseDir%/}' already exists"
-    for_each_repo_owned_by "$UserName" clone_repo
+    for_each_repo clone_repo
 }
 
 ssh_clone_all_repos() {
     [ -z "$UserName" ] && fatal_error 'missing USERNAME parameter'
     [ -e "${BaseDir%/}" ] && fatal_error "directory '${BaseDir%/}' already exists"
-    for_each_repo_owned_by "$UserName" ssh_clone_repo
+    for_each_repo ssh_clone_repo
 }
 
 enumerate_all_repos() {
     [ -z "$UserName" ] && fatal_error 'missing USERNAME parameter'
-    for_each_repo_owned_by "$UserName" enumerate_repo
+    for_each_repo enumerate_repo
 }
 
 debug_all_repos() {
     [ -z "$UserName" ] && fatal_error 'missing USERNAME parameter'
-    for_each_repo_owned_by "$UserName" debug_repo
+    for_each_repo debug_repo
 }
 
 #============================== FOR EACH REPO ===============================#
@@ -77,7 +78,8 @@ debug_all_repos() {
 ## Group of funtions to be used with 'for_each_repo_owned_by'
 ##
 ## @param index         Position of the repo within the list
-## @param name          ???
+## @param name          The name of the repository 
+## @param owner         The login name of the owner of the repository
 ## @param description   A text describing the repo
 ## @param directory     The local directory where the repo will be cloned
 ## @param html_url      The URL of the repository's page on GitHub
@@ -86,22 +88,23 @@ debug_all_repos() {
 ##
 clone_repo() {
     fatal_error "clone repository isn't supported yet"
-    local index=$1 name=$2 description=$3 directory=$4 html_url=$5 clone_url=$6 ssh_url=$7
+    local index=$1 name=$2 owner=$3 description=$4 directory=$5 html_url=$6 clone_url=$7 ssh_url=$8
     $DryRun git clone "$git_pull_url" "$directory"
 }
 ssh_clone_repo() {
     fatal_error "clone repository with ssh isn't supported yet"
-    local index=$1 name=$2 description=$3 directory=$4 html_url=$5 clone_url=$6 ssh_url=$7
+    local index=$1 name=$2 owner=$3 description=$4 directory=$5 html_url=$6 clone_url=$7 ssh_url=$8
     local ssh_url=$(sed "s/^.*:\/\//git@/;s/\//:/" <<<"$git_pull_url")
     $DryRun git clone "$ssh_url" "$directory"
 }
 enumerate_repo() {
-    local index=$1 name=$2 description=$3 directory=$4 html_url=$5 clone_url=$6 ssh_url=$7
+    local index=$1 name=$2 owner=$3 description=$4 directory=$5 html_url=$6 clone_url=$7 ssh_url=$8
     printf "%3d: %-16s %s\n" $index "$name" "$description"
 }
 debug_repo() {
-    local index=$1 name=$2 description=$3 directory=$4 html_url=$5 clone_url=$6 ssh_url=$7
+    local index=$1 name=$2 owner=$3 description=$4 directory=$5 html_url=$6 clone_url=$7 ssh_url=$8
     echo "$index:$name"
+    echo "    owner    : $owner"
     echo "    directory: $directory"
     echo "    descript : $description"
     echo "    webpage  : $html_url"
@@ -111,69 +114,16 @@ debug_repo() {
 }
 
 ## Iterates over all user's repos and execute a function on each one
-##
-## @param username      The username of the repo owner
 ## @param repofunction  The function to execute on each repo
 ##
-for_each_repo_owned_by() {
-    local username=$1 repofunction=$2
+for_each_repo() {
+    local repofunction=$1
     local properties
     
-    # api documentation:
-    # [ https://docs.github.com/en/rest/repos/repos#list-repositories-for-a-user ]
-    
     # super quick and dirty code to parse json with awk
     # kids, don't do it at home!!!
-    IFS=$'\n' read -r -d '' -a properties < <( print_varvalue_repo_data "$username" && printf '\0' )
+    IFS=$'\n' read -r -d '' -a properties < <( print_varvalue_repo_data && printf '\0' )
     proc_repo_properties "$repofunction" "${properties[@]}"
-}
-
-#=================================== MISC ===================================#
-
-## Prints data from all repositories in var/value format
-print_varvalue_repo_data() {
-    # super quick and dirty code to parse json with awk
-    # kids, don't do it at home!!!
-    print_json_repo_data "$1" | awk '
-        /\{/{++s} /\"topics\"/{t=1} 
-        s==1 && (/\"name\"/||/html_url/||/description/||/clone_url/||/ssh_url/) {
-            sub(/^[ \t]*/,""); sub(/[ ,\t]*$/,""); sub(/\":[ \t]*/,"\"\n"); print
-        }
-        t==1 && /\"dir-/ {
-            match($0,/\"dir-[^\"]*\"/); print "\"topic\"\n" substr($0,RSTART,RLENGTH)
-        }
-        /\}/{--s} /]/{t=0} s==0 { print "}" }
-        '
-}
-
-## Prints data from all repositories in JSON format
-print_json_repo_data() {
-    local username="$1"
-    local wget
-    if   hash wget &>/dev/null; then wget=( wget --quiet -O- )
-    elif hash curl &>/dev/null; then wget=( curl --silent --fail --location )
-    else fatal_error "curl or wget must be installed in the system"
-    fi
-    if [ ${#username} -ge 36 ] && [ "${username:0:2}" = gh ]; then
-        "${wget[@]}" \
-          --header "Accept: application/vnd.github.v3+json" \
-          --header "Authorization: token $username"         \
-          "https://api.github.com/user/repos"
-    else
-        "${wget[@]}" \
-          --header "Accept: application/vnd.github.v3+json" \
-          "https://api.github.com/users/$username/repos"
-    fi    
-}
-
-## Prints the path for the local directory where repository will be cloned
-print_local_directory() {
-    local name=$1 topic=$2 subdir
-    case $Group in
-        --by-list)  subdir="$list/" ;;
-        --by-topic) [ ! -z "$topic" ] && subdir="$topic/" ;;
-    esac
-    echo "${BaseDir}${subdir}${name}"
 }
 
 ## Executes a function on every repo reported by the provided properties
@@ -194,7 +144,7 @@ print_local_directory() {
 proc_repo_properties() {
     local repofunction=$1
     local index=0 topic
-    local name html_url description clone_url ssh_url
+    local name owner html_url description clone_url ssh_url
     local remove_quotes='sub(/^\"/,"");sub(/\"$/,"")'
     local remove_dir_prefix='sub(/^"dir-/,"");sub(/\"$/,"")'
     local capitalize='print toupper(substr($0,0,1))tolower(substr($0,2))'
@@ -205,6 +155,9 @@ proc_repo_properties() {
         case "$1" in
             '"name"')
               shift; name=$(awk "{$remove_quotes}1" <<<"$1")
+              ;;
+            '"login"')
+              shift; owner=$(awk "{$remove_quotes}1" <<<"$1")
               ;;
             '"description"')
               shift; description=$1
@@ -223,14 +176,71 @@ proc_repo_properties() {
               ;;
             '}')
             if [ ! -z "$name" -a ! -z "$clone_url" -a ! -z "$ssh_url" ]; then
-                directory=$(print_local_directory "$name" "$topic")
+                directory=$(print_local_directory $index "$name" "$owner" "$topic")
                 ((index++))
-                "$repofunction" $index "$name" "$description" "$directory" "$html_url" "$clone_url" "$ssh_url"
-                name=;description=;directory=;html_url=;clone_url=;ssh_url=;topic=
+                "$repofunction" $index "$name" "$owner" "$description" "$directory" "$html_url" "$clone_url" "$ssh_url"
+                name=;owner=;description=;directory=;html_url=;clone_url=;ssh_url=;topic=
             fi
         esac
         shift
     done
+}
+
+#=============================== GITHUB API ================================#
+
+## Prints data from all repositories in var/value format
+## [ https://docs.github.com/en/rest/repos/repos ]
+print_varvalue_repo_data() {
+    # super quick and dirty code to parse json with awk
+    # kids, don't do it at home!!!
+    print_json_repo_data | awk '
+        /\{/{++s} /\"topics\"/{t=1} 
+        (s==1 && (/\"name\"/||/\"html_url\"/||/\"description\"/||/\"clone_url\"/||/\"ssh_url\"/)) ||
+        (s==2 && (/\"login"/)) {
+            sub(/^[ \t]*/,""); sub(/[ ,\t]*$/,""); sub(/\":[ \t]*/,"\"\n"); print
+        }
+        t==1 && /\"dir-/ {
+            match($0,/\"dir-[^\"]*\"/); print "\"topic\"\n" substr($0,RSTART,RLENGTH)
+        }
+        /\}/{--s} /]/{t=0} s==0 { print "}" } '
+}
+
+## Prints data from all repositories in JSON format
+## [ https://docs.github.com/en/rest/repos/repos ]
+print_json_repo_data() {
+    local wget
+    if   hash wget &>/dev/null; then wget=( wget --quiet -O- )
+    elif hash curl &>/dev/null; then wget=( curl --silent --fail --location )
+    else fatal_error "curl or wget must be installed in the system"
+    fi
+    if [ ! -z "$UserToken" ]; then
+        "${wget[@]}" \
+          --header "Accept: application/vnd.github.v3+json" \
+          --header "Authorization: token $UserToken"         \
+          "https://api.github.com/user/repos"
+    else
+        "${wget[@]}" \
+          --header "Accept: application/vnd.github.v3+json" \
+          "https://api.github.com/users/$UserName/repos"
+    fi    
+}
+
+#================================== MISC ===================================#
+
+## Prints the path for the local directory where repository will be cloned
+print_local_directory() {
+    local index=$1 reponame=$2 owner=$3 topic=$4
+    local root group_dir
+    case $Group in
+        --by-list)  group_dir="$list/" ;;
+        --by-topic) [ ! -z "$topic" ] && group_dir="${topic%/}/" ;;
+    esac
+    if   [ ! -z "$BaseDir"   ];                      then root="${BaseDir%/}/"
+    elif [ ! -z "$UserToken" ] && [ ! -z "$owner" ]; then root="./${owner%/}/"
+    elif [ ! -z "$UserName"  ];                      then root="./${UserName%/}/"
+    else                                                  root="./GitHub/"
+    fi
+    echo "${root}${group_dir}${reponame}"
 }
 
 #================================== START ===================================#
@@ -257,9 +267,10 @@ while test $# -gt 0; do
     shift
 done
 
-# update base directory
-[ -z "$BaseDir" ] && BaseDir="./$UserName/"
-BaseDir=${BaseDir%/}/
+# update UserName / UserToken
+if [ "${UserName:0:2}" = gh ] && [ ${#UserName} -ge 36 ]; then
+    UserToken="$UserName"
+fi
 
 # handle unimplemented options
 [ "$Group" == '--by-list' ] && fatal_error "group by stars list isn't implemented yet"
